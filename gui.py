c294713d05a4e139ee9f6a6464c80af0c2ddf77f
berrajo <berraj97@gmail.com>
2018-05-23 21:45:55 +0200

    added comments to the code

--- gui.py
+++ gui.py
@@ -14,25 +14,33 @@ from PIL import ImageTk
 from imutils.object_detection import non_max_suppression
 from imutils import paths
  
+# original image panel 
 panelA = None
+# error-prone image panel
 panelB = None
+# final image panel
 panelC = None
 
 def select_image():
+
 	# grab a reference to the image panels
 	global panelA, panelB, panelC
  
-	# open a file chooser dialog and allow the user to select an input
-	# image
+	# open a file chooser dialog and allow the user 
+	# to select an input image
 	path = fd.askopenfilename()
 
+	# check if the path is valid
 	if len(path) > 0:
 
+		# initialize the HOG descriptor/person detector
 		hog = cv2.HOGDescriptor()
 		hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+		# load the image and resize it to fit the panel
 		image = cv2.imread(path)
 		image = imutils.resize(image, width=min(400, image.shape[1]))
+		# create copies of the original to process
 		original = image.copy()
 		initial = image.copy()
 		
@@ -56,10 +64,12 @@ def select_image():
 		filename = path[path.rfind("/") + 1:]
 		print("[INFO] {}: {} initial boxes, {} after suppression".format(filename, len(rects), len(pick)))
 
+		# change the color model of the images from BGR 2 RGB to allow further processing
 		original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
 		initial = cv2.cvtColor(initial, cv2.COLOR_BGR2RGB)
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+		# loads the image to memory
 		original = Image.fromarray(original)
 		initial = Image.fromarray(initial)
 		image = Image.fromarray(image)
@@ -69,7 +79,8 @@ def select_image():
 		image = ImageTk.PhotoImage(image)
 
 	if panelA is None or panelB is None or panelC is None:
-
+		
+		# place the panels on the main gui frame
 		panelA = Label(image=original)
 		panelA.image = original
 		panelA.pack(side="left", padx=10, pady=10)
@@ -82,7 +93,7 @@ def select_image():
 		panelC.image = image
 		panelC.pack(side="right", padx=10, pady=10)
 	else:
-		
+		# place the final images on the panels
 		panelA.configure(image=original)
 		panelB.configure(image=initial)
 		panelC.configure(image=image)
@@ -93,20 +104,27 @@ def select_image():
 
 def main():
 
+	# construct the argument parser and parse the arguments
 	#ap = argparse.ArgumentParser()
 	#ap.add_argument("-i", "--images", required=True, help="path to images directory")
 	#args = vars(ap.parse_args())
 
+	# initialize the HOG descriptor/person detector
 	#hog = cv2.HOGDescriptor()
 	#hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+	# loop over the image paths
 	#imagePaths = list(paths.list_images(args["images"]))
 
+	# the main gui fram
 	root = Tk()
 
+	# button allowing selection of the image
 	btn = Button(root, text="Select an image", command=select_image)
+	# add the button to the main frame
 	btn.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
 
+	# run the main gui loop
 	root.mainloop()
 
 if __name__ == "__main__":
--- pedestrian_detection.py
+++ pedestrian_detection.py
@@ -1,6 +1,7 @@
 # Usage
 # python3 pedestrian_detection.py -i images
 
+# import the necessary packages
 from __future__ import print_function
 from imutils.object_detection import non_max_suppression
 from imutils import paths
